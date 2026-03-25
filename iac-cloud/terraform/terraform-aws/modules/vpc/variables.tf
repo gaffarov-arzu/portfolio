14fb5c43abbb24cd4b0f8291f659c744c551c54a
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2026-03-25 11:20:31 +0400

    Create variables.tf

---
+++ iac-cloud/terraform/terraform-aws/modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+variable "cidr_block" {}
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "availability_zones" { type = list(string) }
