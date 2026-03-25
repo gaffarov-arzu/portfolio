7ce13b227b648c89b60a7574b33bfe3b58af629d
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2026-03-25 11:40:46 +0400

    Update outputs.tf

--- iac-cloud/terraform/terraform-aws/environments/dev/outputs.tf
+++ iac-cloud/terraform/terraform-aws/environments/dev/outputs.tf
@@ -1,1 +1,15 @@
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
 
+output "s3_bucket_name" {
+  value = module.s3.bucket_name
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
