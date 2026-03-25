55c8887092a1bdad7c81ce070ab7384f70340422
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2026-03-25 11:34:27 +0400

    Update and rename 2 to main.tf

---
+++ iac-cloud/terraform/terraform-aws/modules/ec2/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "main" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name        = "${var.project_name}-ec2"
+    Environment = var.environment
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name   = "${var.project_name}-ec2-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
