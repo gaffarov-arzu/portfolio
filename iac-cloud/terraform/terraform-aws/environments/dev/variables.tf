efa28fdc1b85a75f3d9d459820250e202d336818
Arzu Qafarov <156705532+gaffarov-arzu@users.noreply.github.com>
2026-03-25 11:39:29 +0400

    Update variables.tf

--- iac-cloud/terraform/terraform-aws/environments/dev/variables.tf
+++ iac-cloud/terraform/terraform-aws/environments/dev/variables.tf
@@ -1,1 +1,15 @@
-
+variable "aws_region" { default = "us-east-1" }
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "availability_zones" { type = list(string) }
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "db_instance_class" {}
+variable "db_allocated_storage" { type = number }
+variable "db_name" {}
+variable "db_username" {}
+variable "db_password" { sensitive = true }
